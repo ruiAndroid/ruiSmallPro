03e061e987365082ac658b2e592edef39d111215
[rui] <[jianrui_android@163.com]>
2018-04-17 18:37:02 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.   On branch master
      Initial commit
    
      Changes to be committed:
     	new file:   app.js
     	new file:   app.json
     	new file:   app.wxss
     	new file:   pages/ganhuo/ganhuo.js
     	new file:   pages/ganhuo/ganhuo.json
     	new file:   pages/ganhuo/ganhuo.wxml
     	new file:   pages/ganhuo/ganhuo.wxss
     	new file:   pages/ganhuo/ganhuoDetails.js
     	new file:   pages/ganhuo/ganhuoDetails.json
     	new file:   pages/ganhuo/ganhuoDetails.wxml
     	new file:   pages/ganhuo/ganhuoDetails.wxss
     	new file:   pages/index/index.js
     	new file:   pages/index/index.wxml
     	new file:   pages/index/index.wxss
     	new file:   pages/logs/logs.js
     	new file:   pages/logs/logs.json
     	new file:   pages/logs/logs.wxml
     	new file:   pages/logs/logs.wxss
     	new file:   project.config.json
     	new file:   utils/util.js

---
+++ pages/ganhuo/ganhuo.js
@@ -0,0 +1,101 @@
+// pages/ganhuo/ganhuo.js
+var that
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [],
+    //是否显示toast
+    hiddenToast: true
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    that=this
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    getData(false)
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    getData(true)
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
+
+//请求网络数据，isPul区分用户是否下拉操作
+var getData = function (isPull) {
+  //请求干货列表的数据
+  wx.request({
+    url: 'https://lion.tuyangdanci.com/v1/article/list',      //接口地址
+    method: 'POST', //必须为大写（例如：POST）
+    data: {
+      user_id: "5aaa65db362d0e529c08470a",
+
+    },//请求的参数
+    header: { //请求头,设置为默认值即可
+      'content-type': 'application/json' // 默认值
+    },
+    //请求成功的回调
+    success: function (res) {
+      this.list = res.data; //把取到的数组的值赋值给list
+      console.log(this.list)
+      console.log("渲染数据")
+      console.log("数据长度:" + this.list.data.length)
+      that.setData({   //这里调用setData时不能用this.setData，会报错
+        list: this.list.data
+      })
+    }
+  })
+  if (isPull) {
+    that.setData({
+      hiddenToast:false
+    })
+    //停止下拉
+    wx.stopPullDownRefresh()
+  }
+
+}
---
+++ pages/ganhuo/ganhuoDetails.js
@@ -0,0 +1,70 @@
+// pages/ganhuo/ganhuoDetails.js
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title:"",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.title)
+    this.title=options.title
+    this.setData({
+      title:this.title
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+      console.log("下拉了")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
