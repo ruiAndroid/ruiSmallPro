f25b21abd630efd035449920eb6cf8368bc69a06
[rui] <[jianrui_android@163.com]>
2018-04-20 18:23:13 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.   On branch master   Your branch is up-to-date with 'origin/master'.
      Changes to be committed:
     	modified:   app.json
     	new file:   data/post-data.js
     	new file:   images/avatar/1.png
     	new file:   images/avatar/2.png
     	new file:   images/avatar/3.png
     	new file:   images/avatar/4.png
     	new file:   images/avatar/5.png
     	new file:   images/icon/chat1.png
     	new file:   images/icon/view.png
     	new file:   images/iqiyi.png
     	new file:   images/jia.png
     	new file:   images/post/bl.png
     	new file:   images/post/cat.png
     	new file:   images/post/crab.png
     	new file:   images/post/sls.JPG
     	new file:   images/post/vr.png
     	new file:   images/post/xiaolong.jpg
     	modified:   pages/index/index.js
     	modified:   pages/index/index.wxml
     	new file:   pages/posts/post-detail/post-detail.js
     	new file:   pages/posts/post-detail/post-detail.json
     	new file:   pages/posts/post-detail/post-detail.wxml
     	new file:   pages/posts/post-detail/post-detail.wxss
     	new file:   pages/posts/post-item/post-item-template.wxml
     	new file:   pages/posts/post-item/post-item-template.wxss
     	new file:   pages/posts/post.js
     	new file:   pages/posts/post.json
     	new file:   pages/posts/post.wxml
     	new file:   pages/posts/post.wxss

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,14 @@ const app = getApp()
 
 Page({
   data: {
-  }
 
+  }
+  //跳转到新闻列表详情
+  ,toPostLists: function (event) {
+      console.log("跳转到新闻程序列表")
+      wx.navigateTo({
+        //需要跳转的界面路径
+        url: '/pages/posts/post',
+      })
+  }
 })
---
+++ pages/posts/post.js
@@ -0,0 +1,81 @@
+// pages/posts/post.js
+var postData=require('../../data/post-data.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //options为页面跳转带来的参数
+    console.log(postData)
+    this.setData({
+      post_content: postData.postList
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  //跳转到文章详情界面
+  toPostDetails:function(event){
+    var postId=event.currentTarget.dataset.postid
+    console.log("post id is:"+postId);
+    wx.navigateTo({
+      url: 'post-detail/post-detail',
+    })
+
+  }
+})
